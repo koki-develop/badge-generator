4999fd237049448ad999bc11d05012d7759a0ce5
koki-develop <kou.pg.0131@gmail.com>
2022-09-29 08:39:51 +0900

    deploy 処理も追加

--- bin/push.sh
+++ bin/push.sh
@@ -3,9 +3,16 @@
 set -euo pipefail
 
 readonly GCP_PROJECT_ID=badge-generator
-readonly REGISTRY_HOST=asia-northeast1-docker.pkg.dev
-readonly DOCKER_IMAGE=${REGISTRY_HOST}/${GCP_PROJECT_ID}/app/frontend
+readonly REGION=asia-northeast1
+readonly REGISTRY_HOST=${REGION}-docker.pkg.dev
+readonly DOCKER_IMAGE=${REGISTRY_HOST}/${GCP_PROJECT_ID}/app/frontend:latest
 
 gcloud auth configure-docker "${REGISTRY_HOST}" --quiet
 docker build -t "${DOCKER_IMAGE}" --platform=linux/amd64 ./frontend
 docker push "${DOCKER_IMAGE}"
+
+gcloud run deploy frontend \
+  --image="${DOCKER_IMAGE}" \
+  --region="${REGION}" \
+  --project="${GCP_PROJECT_ID}"
+gcloud run services update-traffic frontend --to-latest --region="${REGION}"
