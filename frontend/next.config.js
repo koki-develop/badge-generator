31a025b0d69d28013f7f8b5cab26f0edf9eb6d08
koki-develop <kou.pg.0131@gmail.com>
2022-09-29 11:14:59 +0900

    svg を import できるようにした

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: "standalone",
+  webpack: (config) => {
+    config.module.rules.push({
+      resourceQuery: { not: [/raw/] },
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    config.module.rules.push({
+      resourceQuery: /raw/,
+      type: "asset/source",
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
