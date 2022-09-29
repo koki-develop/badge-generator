073a9c9ad662073927bb521f16141a9321edff37
koki-develop <kou.pg.0131@gmail.com>
2022-09-29 21:32:00 +0900

    api/ に img/ でアクセスできるようにした

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: "standalone",
+  async rewrites() {
+    return [
+      {
+        source: "/img/:path*",
+        destination: "/api/:path*",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
