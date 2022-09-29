f7c95eba9e4c41cd0f13b712fa688420ddcaea8c
koki-develop <kou.pg.0131@gmail.com>
2022-09-29 18:59:34 +0900

    renderZennBadge を実装

---
+++ frontend/src/lib/zenn.ts
@@ -0,0 +1,67 @@
+import axios from "axios";
+import zenn from "../logos/zenn.svg?raw";
+import { renderBadge, RenderBadgeOptions } from "./badge";
+
+export type BadgeType = "likes";
+
+const typeLabelMap: Record<BadgeType, string> = {
+  likes: "Likes",
+};
+
+export type RenderZennBadgeOptions = Omit<
+  RenderBadgeOptions,
+  "color" | "logoDataUrl" | "message" | "label"
+> & {
+  type: BadgeType;
+  username: string;
+};
+
+export const renderZennBadge = async (
+  options: RenderZennBadgeOptions
+): Promise<string> => {
+  const { type, username, ...renderOptions } = options;
+  const value = await _getValue(type, username);
+
+  const svg = renderBadge({
+    logoDataUrl: `data:image/svg+xml;base64,${Buffer.from(zenn).toString(
+      "base64"
+    )}`,
+    color: "#3EA8FF",
+    label: typeLabelMap["likes"],
+    message: value.toString(),
+    ...renderOptions,
+  });
+
+  return svg;
+};
+
+const _getValue = async (
+  type: BadgeType,
+  username: string
+): Promise<number> => {
+  switch (type) {
+    case "likes":
+      return _getLikesCount(username);
+  }
+};
+
+const _getLikesCount = async (username: string): Promise<number> => {
+  const { total_liked_count } = await _getUser(username);
+  return total_liked_count;
+};
+
+type User = {
+  articles_count: number;
+  books_count: number;
+  follower_count: number;
+  scraps_count: number;
+  total_liked_count: number;
+};
+
+// TODO: cache 処理実装
+const _getUser = async (username: string): Promise<User> => {
+  const endpoint = `https://zenn.dev/api/users/${username}`;
+  const { data } = await axios.get<{ user: User }>(endpoint);
+
+  return data.user;
+};
