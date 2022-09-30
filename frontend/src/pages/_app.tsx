65be0b91ed3978853dec786ebe5e6e90a2dac458
koki-develop <kou.pg.0131@gmail.com>
2022-09-30 14:00:54 +0900

    諸々見た目修正

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -6,7 +6,7 @@ import "../styles/global.css";
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <div className="min-h-screen">
-      <div className="px-2 sm:px-4">
+      <div className="px-4">
         <div className="container mx-auto">
           <Link href="/">
             <a>
@@ -16,13 +16,13 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         </div>
       </div>
 
-      <main className="mb-4 px-2 sm:px-4">
+      <main className="mb-4 px-4">
         <div className="container mx-auto">
           <Component {...pageProps} />
         </div>
       </main>
 
-      <div className="px-2 sm:px-4">
+      <div className="px-4">
         <div className="container mx-auto flex flex-col items-center justify-center text-gray-500">
           <div className="mb-2">
             <p>&copy;2022 Koki Sato</p>
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,22 +2,37 @@ import classNames from "classnames";
 import { Disclosure } from "@headlessui/react";
 import type { NextPage } from "next";
 import React from "react";
-import { AiOutlineCopy } from "react-icons/ai";
+import { AiOutlineDown, AiOutlineUp, AiOutlineCopy } from "react-icons/ai";
+import { BsChevronDown, BsChevronUp } from "react-icons/bs";
 
 const Home: NextPage = () => {
   const source = "https://badgen.org/img/zenn/kou_pg_0131/likes";
 
   return (
-    <div>
+    <div className="pt-4">
       <div>
-        <h2 className="text-xl">Zenn</h2>
+        <div>
+          <h2 className="text-2xl font-bold">Zenn</h2>
+        </div>
 
-        <div className="mb-2 flex flex-col">
-          <label className="text-sm">ユーザー名</label>
-          <input
-            className="w-full rounded border px-2 py-1 outline-none sm:w-1/3"
-            type="text"
-          />
+        <div className="mb-4">
+          <div className="mb-2 flex flex-col">
+            <label className="text-sm">ユーザー名</label>
+            <input
+              className="h-[38px] w-full rounded border px-2 outline-none sm:w-1/3"
+              type="text"
+            />
+          </div>
+
+          <div className="mb-2 flex flex-col">
+            <label className="text-sm">スタイル</label>
+            <select className="h-[38px] w-full rounded border px-2 outline-none hover:bg-gray-50 sm:w-1/3">
+              <option value="" selected>
+                plastic
+              </option>
+              <option value="">flat</option>
+            </select>
+          </div>
         </div>
 
         <Disclosure>
@@ -25,20 +40,26 @@ const Home: NextPage = () => {
             <>
               <Disclosure.Button
                 className={classNames(
-                  "flex w-full items-center rounded border px-4 py-2",
+                  "flex w-full items-center justify-between rounded border px-4 py-2 hover:bg-gray-50 active:bg-gray-100",
                   { "rounded-b-none": open }
                 )}
               >
-                <h3 className="mr-2">Likes</h3>
-                <span>
-                  <img src={source} alt="" />
+                <div className="flex items-center">
+                  <h3 className="mr-2">Likes</h3>
+                  <span className="mr-2">
+                    <img src={source} alt="Badge" />
+                  </span>
+                </div>
+                <span className="text-sm">
+                  {open && <BsChevronUp />}
+                  {!open && <BsChevronDown />}
                 </span>
               </Disclosure.Button>
               <Disclosure.Panel className="rounded-b border border-t-0 p-4 pt-2">
                 <div>
                   <label className="text-xs">Markdown</label>
                   <div className="mb-1 flex">
-                    <button className="rounded-l border border-r-0 p-2 outline-none hover:bg-gray-100 active:bg-gray-200">
+                    <button className="rounded-l border border-r-0 p-2 outline-none hover:bg-gray-50 active:bg-gray-100">
                       <AiOutlineCopy />
                     </button>
                     <input
