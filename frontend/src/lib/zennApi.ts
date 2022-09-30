c368ab6d9d5687a57e84b955ca4f02bceed52a17
koki-develop <kou.pg.0131@gmail.com>
2022-09-30 17:51:50 +0900

    zennApi module を作成

---
+++ frontend/src/lib/zennApi.ts
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { loadCache, saveCache } from "./cache";
+
+type ZennUser = {
+  articles_count: number;
+  books_count: number;
+  follower_count: number;
+  scraps_count: number;
+  total_liked_count: number;
+};
+
+export const getLikesCount = async (
+  username: string
+): Promise<number | null> => {
+  const user = await _getUser(username);
+  return user?.total_liked_count ?? null;
+};
+
+export const getArticlesCount = async (
+  username: string
+): Promise<number | null> => {
+  const user = await _getUser(username);
+  return user?.articles_count ?? null;
+};
+
+const _getUser = async (username: string): Promise<ZennUser | null> => {
+  const cacheKey = _cacheKey(username);
+  const cache = await loadCache<ZennUser>(cacheKey);
+  if (cache?.data) {
+    return cache.data;
+  }
+
+  const endpoint = `https://zenn.dev/api/users/${encodeURIComponent(username)}`;
+  const resp = await axios.get<{ user: ZennUser }>(endpoint, {
+    validateStatus: (status) => [200, 404].includes(status),
+  });
+  if (resp.status === 404) {
+    await saveCache(cacheKey, null);
+    return null;
+  }
+
+  await saveCache(cacheKey, resp.data.user);
+  return resp.data.user;
+};
+
+const _cacheKey = (username: string): string => {
+  return `zenn_${username}`;
+};
