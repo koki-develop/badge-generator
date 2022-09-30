f0c8207859871d766fbc8bf389444cd00ac48de7
koki-develop <kou.pg.0131@gmail.com>
2022-09-30 13:03:18 +0900

    tailwind.config.js から flowbite を削除

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [],
 };
