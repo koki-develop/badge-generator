363405b53c9824a51e1ebd86f58f375c2d46a3bd
koki-develop <kou.pg.0131@gmail.com>
2022-09-30 19:50:47 +0900

    InputWithCopy コンポーネントを作成、 BadgeBlock に適用

--- frontend/src/components/pages/HomePage/BadgeBlock.tsx
+++ frontend/src/components/pages/HomePage/BadgeBlock.tsx
@@ -1,6 +1,6 @@
 import React, { memo } from "react";
-import { AiOutlineCopy } from "react-icons/ai";
 import Disclosure from "../../util/Disclosure";
+import InputWithCopy from "../../util/InputWithCopy";
 
 export type Badge = {
   name: string;
@@ -28,32 +28,20 @@ const BadgeBlock: React.FC<BadgeBlockProps> = memo((props) => {
     >
       <div>
         <label className="text-xs">Markdown</label>
-        <div className="mb-1 flex">
-          <button className="rounded-l border border-r-0 p-2 outline-none hover:bg-gray-50 active:bg-gray-100">
-            <AiOutlineCopy />
-          </button>
-          <input
-            className="grow rounded-r border px-2 py-1 text-sm outline-none"
-            style={{ outline: "none" }}
-            type="text"
-            value={`[![${badge.name}](${badge.src})](${badge.link})`}
-            disabled
-          />
-        </div>
+        <InputWithCopy
+          className="w-full text-sm"
+          type="text"
+          value={`[![${badge.name}](${badge.src})](${badge.link})`}
+          disabled
+        />
 
         <label className="text-xs">HTML</label>
-        <div className="flex">
-          <button className="rounded-l border border-r-0 p-2 outline-none hover:bg-gray-100 active:bg-gray-200">
-            <AiOutlineCopy />
-          </button>
-          <input
-            className="grow rounded-r border px-2 py-1 text-sm outline-none"
-            style={{ outline: "none" }}
-            type="text"
-            value={`<a href="${badge.link}"><img src="${badge.src}" alt="${badge.name}" /></a>`}
-            disabled
-          />
-        </div>
+        <InputWithCopy
+          className="w-full text-sm"
+          type="text"
+          value={`<a href="${badge.link}"><img src="${badge.src}" alt="${badge.name}" /></a>`}
+          disabled
+        />
       </div>
     </Disclosure>
   );
---
+++ frontend/src/components/util/InputWithCopy.tsx
@@ -0,0 +1,29 @@
+import classNames from "classnames";
+import React, { memo } from "react";
+import { AiOutlineCopy } from "react-icons/ai";
+
+export type InputWithCopyProps = React.HTMLProps<HTMLInputElement>;
+
+const InputWithCopy: React.FC<InputWithCopyProps> = memo((props) => {
+  const { ...inputProps } = props;
+
+  return (
+    <span className="flex">
+      <button className="rounded-l border border-r-0 p-2 outline-none hover:bg-gray-50 active:bg-gray-100">
+        <AiOutlineCopy />
+      </button>
+      <input
+        {...inputProps}
+        className={classNames(
+          inputProps.className,
+          "rounded-r border px-2 outline-none"
+        )}
+        type="text"
+      />
+    </span>
+  );
+});
+
+InputWithCopy.displayName = "BadgeBlock";
+
+export default InputWithCopy;
