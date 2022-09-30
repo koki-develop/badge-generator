75549cc20972e88bbfd84f88459e466c5b91bcda
koki-develop <kou.pg.0131@gmail.com>
2022-09-30 17:13:19 +0900

    QiitaBadgeBlocks を作成

--- frontend/src/components/pages/HomePage/HomePage.tsx
+++ frontend/src/components/pages/HomePage/HomePage.tsx
@@ -1,11 +1,16 @@
 import type { NextPage } from "next";
 import React from "react";
+import QiitaBadgeBlocks from "./QiitaBadgeBlocks";
 import ZennBadgeBlocks from "./ZennBadgeBlocks";
 
 const HomePage: NextPage = () => {
   return (
     <div className="pt-4">
       <ZennBadgeBlocks />
+
+      <hr className="my-8" />
+
+      <QiitaBadgeBlocks />
     </div>
   );
 };
