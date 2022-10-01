11bb5d64661ad1ad62c55a38a5104128b9dac141
koki-develop <kou.pg.0131@gmail.com>
2022-10-01 09:43:05 +0900

    .eslintrc.js を作成

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ["next/core-web-vitals", "prettier"],
+  plugins: ["unused-imports", "@typescript-eslint"],
+  rules: {
+    "unused-imports/no-unused-imports": "error",
+    "@typescript-eslint/no-unused-vars": "error",
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          ["parent", "sibling"],
+          "object",
+          "type",
+          "index",
+        ],
+        pathGroupsExcludedImportTypes: ["builtin"],
+        alphabetize: { order: "asc", caseInsensitive: true },
+      },
+    ],
+  },
+};
