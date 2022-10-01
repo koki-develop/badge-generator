e997e2a1464ddcce6e2bc75dd8b403f545d98880
koki-develop <kou.pg.0131@gmail.com>
2022-10-01 10:36:12 +0900

    Google Analytics を導入

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,8 +1,26 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import Layout from "@/components/Layout";
 import type { AppProps } from "next/app";
 import "@/styles/global.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [mounted, setMounted] = useState<boolean>(false);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  useEffect(() => {
+    if (!mounted) return;
+    if (!process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID) return;
+    window.gtag("config", process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID, {
+      page_path: router.pathname,
+    });
+  }, [mounted, router.pathname]);
+
   return (
     <Layout>
       <Component {...pageProps} />
---
+++ frontend/src/pages/_document.tsx
@@ -0,0 +1,37 @@
+import NextDocument, { Head, Html, Main, NextScript } from "next/document";
+import React from "react";
+
+export default class Document extends NextDocument {
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          {/* Google tag (gtag.js) */}
+          {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+                  `,
+                }}
+              />
+            </>
+          )}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
