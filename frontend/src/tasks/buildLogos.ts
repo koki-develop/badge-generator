73a3ed83cc4f3c29965afc800acc3d39ddb30b9d
koki-develop <kou.pg.0131@gmail.com>
2022-10-02 18:20:47 +0900

    buildLogos をリファクタ

--- frontend/src/tasks/buildLogos.ts
+++ frontend/src/tasks/buildLogos.ts
@@ -1,18 +1,34 @@
 import fs from "fs";
 import path from "path";
 
-const load = (pathname: string): string => {
+const _readAsBase64 = (...paths: string[]): string => {
   return fs
-    .readFileSync(path.resolve(process.cwd(), pathname))
+    .readFileSync(path.resolve(process.cwd(), ...paths))
     .toString("base64");
 };
 
-const zenn = load("public/logos/zenn.svg");
-const qiita = load("public/logos/qiita.png");
-const atcoderBlack = load("public/logos/atcoder_black.svg");
-const atcoderWhite = load("public/logos/atcoder_white.svg");
+const _base64ToDataUrl = (base64: string, mime: string): string =>
+  `data:${mime};base64,${base64}`;
 
-fs.writeFileSync(
-  "src/logos.json",
-  JSON.stringify({ zenn, qiita, atcoderBlack, atcoderWhite })
+const mime = {
+  png: "image/png",
+  svg: "image/svg+xml",
+} as const;
+
+const logos = {
+  zenn: { filename: "zenn.svg", mime: mime.svg },
+  qiita: { filename: "qiita.png", mime: mime.png },
+  atcoderBlack: { filename: "atcoder_black.svg", mime: mime.svg },
+  atcoderWhite: { filename: "atcoder_white.svg", mime: mime.svg },
+};
+
+const dataUrls = Object.entries(logos).reduce<Record<string, string>>(
+  (result, [name, logo]) => {
+    const base64 = _readAsBase64("public/logos", logo.filename);
+    result[name] = _base64ToDataUrl(base64, logo.mime);
+    return result;
+  },
+  {}
 );
+
+fs.writeFileSync("src/logos.json", JSON.stringify(dataUrls));
