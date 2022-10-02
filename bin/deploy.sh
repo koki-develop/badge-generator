d6757923cde5e0cd9857c032b615bdcca2eda1bd
koki-develop <kou.pg.0131@gmail.com>
2022-10-02 15:45:40 +0900

    Revert "[debug] 一旦 build, push ステップを削除"
    This reverts commit ad5fba16e02ce9e53f5e326dbc3a48e6e04931cb.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,13 +7,13 @@ readonly REGION=asia-northeast1
 readonly REGISTRY_HOST=${REGION}-docker.pkg.dev
 readonly DOCKER_IMAGE=${REGISTRY_HOST}/${GCP_PROJECT_ID}/app/frontend:latest
 
-# gcloud auth configure-docker "${REGISTRY_HOST}" --quiet
-# docker build \
-#   -t "${DOCKER_IMAGE}" \
-#   --platform=linux/amd64 \
-#   --build-arg GA_MEASUREMENT_ID="${GA_MEASUREMENT_ID}" \
-#   ./frontend
-# docker push "${DOCKER_IMAGE}"
+gcloud auth configure-docker "${REGISTRY_HOST}" --quiet
+docker build \
+  -t "${DOCKER_IMAGE}" \
+  --platform=linux/amd64 \
+  --build-arg GA_MEASUREMENT_ID="${GA_MEASUREMENT_ID}" \
+  ./frontend
+docker push "${DOCKER_IMAGE}"
 
 gcloud run deploy frontend \
   --image="${DOCKER_IMAGE}" \
