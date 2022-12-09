71fc45e4c8c4ce8b1a85d357e7e0d96054418132
koki-develop <kou.pg.0131@gmail.com>
2022-12-09 22:12:25 +0900

    サービスアカウントにロールを設定

--- terraform/github-actions/sa.tf
+++ terraform/github-actions/sa.tf
@@ -1,3 +1,19 @@
 resource "google_service_account" "main" {
   account_id = "gh-actions"
 }
+
+locals {
+  sa_roles = [
+    "roles/iam.serviceAccountUser",
+    "roles/artifactregistry.writer",
+    "roles/run.developer",
+  ]
+}
+
+resource "google_project_iam_binding" "service_account" {
+  for_each = toset(local.sa_roles)
+
+  project = data.google_project.main.id
+  role    = each.value
+  members = ["serviceAccount:${google_service_account.main.email}"]
+}
