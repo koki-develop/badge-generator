ca593f639dce461944932caec6904608425e6f74
koki-develop <kou.pg.0131@gmail.com>
2023-01-03 21:51:43 +0900

    removed @google-cloud/logging-winston

--- frontend/src/lib/logger.ts
+++ frontend/src/lib/logger.ts
@@ -1,9 +1,6 @@
-import { LoggingWinston } from "@google-cloud/logging-winston";
 import winston from "winston";
-
-const loggingWinston = new LoggingWinston();
 
 export const logger = winston.createLogger({
   level: "info",
-  transports: [new winston.transports.Console(), loggingWinston],
+  transports: [new winston.transports.Console()],
 });
