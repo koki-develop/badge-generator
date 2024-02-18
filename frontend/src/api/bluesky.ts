7387bdec6a78e031d590bda8be3dc0dc55ad1b45
koki-develop <kou.pg.0131@gmail.com>
2024-02-18 10:50:42 +0900

    Implement api for bluesky

---
+++ frontend/src/api/bluesky.ts
@@ -0,0 +1,41 @@
+import { NextApiHandler } from "next";
+import { renderSvg } from "@/api/api";
+import { getProfile } from "@/lib/api/bluesky";
+import logos from "@/logos.json";
+
+export type BlueskyBadgeType = "followers" | "posts";
+
+const _selectLabel = (type: BlueskyBadgeType): string =>
+  ({
+    followers: "Followers",
+    posts: "Posts",
+  }[type]);
+
+const _handler = (type: BlueskyBadgeType): NextApiHandler =>
+  renderSvg(async (query) => {
+    const base = {
+      logo: logos.bluesky,
+      color: "#0285FF",
+      label: _selectLabel(type),
+    };
+
+    const result = await getProfile(query.username);
+    if (result.error) return { ...base, error: result.error };
+
+    const value = (() => {
+      switch (type) {
+        case "followers":
+          return result.data.followersCount ?? 0;
+        case "posts":
+          return result.data.postsCount ?? 0;
+      }
+    })();
+
+    return {
+      ...base,
+      message: value.toString(),
+    };
+  });
+
+export const followers = _handler("followers");
+export const posts = _handler("posts");
---
+++ frontend/src/lib/api/bluesky.ts
@@ -0,0 +1,47 @@
+import { BskyAgent } from "@atproto/api";
+import {
+  ProfileView,
+  ProfileViewDetailed,
+} from "@atproto/api/dist/client/types/app/bsky/actor/defs";
+import { ApiError, ApiResult } from "./api";
+import { withCache } from "./cache";
+import { withRate } from "./rate";
+
+const agent = new BskyAgent({ service: "https://bsky.social" });
+
+export const getProfile = async (
+  handle: string
+): Promise<ApiResult<ProfileViewDetailed>> => _getProfileWithCache(handle);
+
+const _getProfileWithCache = async (
+  handle: string
+): Promise<ApiResult<ProfileView>> => {
+  const cacheKey = `bluesky_${handle}`;
+  return withCache(
+    cacheKey,
+    withRate("bluesky", () => _getProfile(handle))
+  );
+};
+
+const _getProfile = async (
+  handle: string
+): Promise<ApiResult<ProfileViewDetailed>> => {
+  if (!agent.session) {
+    await agent.login({
+      identifier: process.env.BLUESKY_IDENTIFIER!,
+      password: process.env.BLUESKY_PASSWORD!,
+    });
+  }
+
+  return await agent
+    .getProfile({ actor: handle })
+    .then(({ data }) => {
+      return { data };
+    })
+    .catch(() => {
+      return {
+        data: null,
+        error: ApiError.UserNotFound,
+      };
+    });
+};
