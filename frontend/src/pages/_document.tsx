c446edde49de026149c5ee83a2ed07aa68dfdcd2
koki-develop <kou.pg.0131@gmail.com>
2025-06-14 12:07:51 +0900

    fix: npx @next/codemod@canary upgrade latest

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,5 +1,5 @@
 import NextDocument, { Head, Html, Main, NextScript } from "next/document";
-import React from "react";
+import React, { type JSX } from "react";
 
 export default class Document extends NextDocument {
   render(): JSX.Element {
